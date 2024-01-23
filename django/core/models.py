b6a577f5fd358d52efbfe9bb86cc7bb70eb79341
inhwa.son <patate.douce.pomme@gmail.com>
2024-01-23 14:19:12 -0800

    django

---
+++ django/core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
